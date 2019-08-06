bce382f167a54c75efce6061e7460e3d593010b4
Dominique McLemore <macmanco85@gmail.com>
2019-08-06 10:18:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
+  delete newObj[key]
+  return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
-  return object;
+  delete object[key]
+  return object
 }
